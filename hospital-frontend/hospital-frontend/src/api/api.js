bc9e7aa129549a25596b5b077835d5368ab4a3f4
Vaidehi Tiwari <tvaidehi792@gmail.com>
2026-05-26 16:42:01 +0530

    fixed versal and render

--- hospital-frontend/hospital-frontend/src/api/api.js
+++ hospital-frontend/hospital-frontend/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL.replace(/\/$/, '')}/api` 
+    : '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
